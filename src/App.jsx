9f50b902acb2ba81845387abba93351719c694ee
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-07-20 07:31:11 -0400

    Added, styled, and invoked footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Header } from "./components/Header/Header.jsx";
+import { Footer } from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,4 +2,19 @@
 
 import "./Footer.css";
 
-export function Footer() {}
+export function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-text">
+        Created by{" "}
+        <a
+          href="https://github.com/joeypizzas"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          joeypizzas
+        </a>
+      </div>
+    </div>
+  );
+}
